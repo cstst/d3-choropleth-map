d1fca5b022e52ef7528371404e1a48909bef140d
Derek L <derekslarson@gmail.com>
2018-10-24 22:02:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as d3 from 'd3';
+import 'd3-selection-multi';
+import * as topojson from 'topojson';
 import './App.css';
 
 class App extends Component {
+  
+  async componentDidMount() {
+    const JSON_URLS = [
+      'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
+      'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
+    ];
+    const data = await Promise.all(JSON_URLS.map(URL => fetch(URL).then(res => res.json())));
+    this.drawMap({ edData: data[0], coData: data[1] });
+  }
+
+  drawMap({ edData, coData }) {
+    const height = 600;
+    const width = 1200;
+    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+    const geojson = topojson.feature(coData, coData.objects.counties);
+  
+    const svg = d3.select('#graph')
+                  .append('svg')
+                  .attrs({ 
+                    height, 
+                    width 
+    
+                  });
+                
+    svg.selectAll('path')
+       .data(geojson.features)
+       .enter()
+       .append('path')
+       .attr('d', d3.geoPath())
+    
+  }
+
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div id="graph"/>
     );
   }
 }
