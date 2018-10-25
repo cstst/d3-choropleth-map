cd1f99a9a0e11e4f6f2449b1042892737af95404
Derek L <derekslarson@gmail.com>
2018-10-25 22:26:27 +0700

    eslint working finally

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ import * as topojson from 'topojson';
 import './App.css';
 
 class App extends Component {
-  
   async componentDidMount() {
     const JSON_URLS = [
       'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
-      'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
+      'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json',
     ];
     const data = await Promise.all(JSON_URLS.map(URL => fetch(URL).then(res => res.json())));
     this.drawMap(data[0], data[1]);
   }
 
-  drawMap(edData, coData) {
-    d3.select("svg").remove();
+  drawMap = (edData, coData) => {
+    d3.select('svg').remove();
     const height = 900;
     const width = 1200;
-    const margin = { top: 100, right: 120, bottom: 150, left: 120 };
+    const margin = {
+      top: 100, right: 120, bottom: 150, left: 120,
+    };
     const geojson = topojson.feature(coData, coData.objects.counties);
     const svg = d3.select('#graph')
-                  .append('svg')
-                  .attrs({ 
-                    height, 
-                    width,
-                  });
+      .append('svg')
+      .attrs({
+        height,
+        width,
+      });
     /*
     svg.append('rect')
        .attrs({
@@ -37,29 +38,27 @@ class App extends Component {
          stroke: 'red',
          'stroke-width': 5
        });
-    */                
+    */
     svg.append('g')
-       .attrs({
-         id: 'map',
-         transform: `translate(${margin.left}, ${margin.top})`,
-       })
-       .selectAll('path')
-       .data(geojson.features)
-       .enter()
-       .append('path')
-       .attrs({
-         class: 'country',
-         d: d3.geoPath(),
-         'data-education': d => edData.filter(co => co.fips === d.id)[0].bachelorsOrHigher
-         
-       }); 
+      .attrs({
+        id: 'map',
+        transform: `translate(${margin.left}, ${margin.top})`,
+      })
+      .selectAll('path')
+      .data(geojson.features)
+      .enter()
+      .append('path')
+      .attrs({
+        class: 'country',
+        d: d3.geoPath(),
+        'data-education': d => edData.filter(co => co.fips === d.id)[0].bachelorsOrHigher,
+
+      });
   }
 
-
-  
   render() {
     return (
-      <div id="graph"/>
+      <div id="graph" />
     );
   }
 }
