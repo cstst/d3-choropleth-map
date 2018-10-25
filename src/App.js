4c2f35792aee172ed7e3fef7930473606d4f1a5e
Derek L <derekslarson@gmail.com>
2018-10-25 13:31:46 +0700

    eslint fix

--- src/App.js
+++ src/App.js
@@ -1,63 +1,68 @@
 import React, { Component } from 'react';
+import * as topojson from 'topojson';
 import * as d3 from 'd3';
 import 'd3-selection-multi';
-import * as topojson from 'topojson';
 import './App.css';
 
 class App extends Component {
-  
   async componentDidMount() {
     const JSON_URLS = [
       'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
-      'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
+      'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json',
     ];
     const data = await Promise.all(JSON_URLS.map(URL => fetch(URL).then(res => res.json())));
-    setInterval(() => this.drawMap({ edData: data[0], coData: data[1] }), 1000);
+    this.drawMap(data[0], data[1]);
   }
 
-  drawMap({ edData, coData }) {
-    d3.select("svg").remove();
+  drawMap = (edData, coData) => {
     const height = 900;
     const width = 1200;
-    const margin = { top: 100, right: 120, bottom: 150, left: 120 };
-    const geojson = topojson.feature(coData, coData.objects.counties);
+    const margin = {
+      top: 100, right: 120, bottom: 150, left: 120,
+    };
+    const bachData = edData.map(co => co.bachelorsOrHigher);
+    const geoJSON = topojson.feature(coData, coData.objects.counties);
+
+    console.log(d3.min(bachData), d3.max(bachData));
+
+    const colors = [
+      'hsl(0, 50%, 90%)',
+      'hsl(0, 50%, 80%)',
+      'hsl(0, 50static%, 70%)',
+      'hsl(0, 50%, 60%)',
+      'hsl(0, 50%, 50%)',
+      'hsl(0, 50%, 40%)',
+      'hsl(0, 50%, 30%)',
+      'hsl(0, 50%, 20%)',
+    ];
+
     const svg = d3.select('#graph')
-                  .append('svg')
-                  .attrs({ 
-                    height, 
-                    width,
-                  });
-    /*
-    svg.append('rect')
-       .attrs({
-         id: 'border',
-         height,
-         width,
-         fill: 'none',
-         stroke: 'red',
-         'stroke-width': 5
-       });
-    */                
+      .append('svg')
+      .attrs({
+        height,
+        width,
+      });
+
     svg.append('g')
-       .attrs({
-         id: 'map',
-         transform: `translate(${margin.left}, ${margin.top})`,
-       })
-       .selectAll('path')
-       .data(geojson.features)
-       .enter()
-       .append('path')
-       .attrs({
-         d: d3.geoPath(),
-         fill: () => '#' + Math.random().toString(16).slice(-6)
-       }); 
+      .attrs({
+        id: 'map',
+        transform: `translate(${margin.left}, ${margin.top})`,
+      })
+      .selectAll('path')
+      .data(geoJSON.features)
+      .enter()
+      .append('path')
+      .attrs({
+        class: 'county',
+        d: d3.geoPath(),
+        'data-fips': d => d.id,
+        'data-education': d => edData.filter(co => co.fips === d.id)[0].bachelorsOrHigher,
+      });
   }
 
-
-  
   render() {
     return (
-      <div id="graph"/>
+      <div id="graph" />
     );
   }
 }
